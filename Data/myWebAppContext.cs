eca3dcbf3f9f66a63939f57fdc193d5f03e7e2b8
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-20 10:44:26 -0400

    adding new project

---
+++ Data/myWebAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace myWebApp.Models
+{
+    public class myWebAppContext : DbContext
+    {
+        public myWebAppContext (DbContextOptions<myWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<myWebApp.Models.Movie> Movie { get; set; }
+    }
+}
