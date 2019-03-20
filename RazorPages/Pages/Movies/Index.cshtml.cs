d1de1855928d355a23be99aa20d9568daa162592
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-20 15:00:06 -0400

    added search

--- RazorPages/Pages/Movies/Index.cshtml.cs
+++ RazorPages/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPages.Models;
 
@@ -20,9 +21,31 @@ namespace RazorPages.Pages.Movies
 
         public IList<Movie> Movie { get;set; }
 
+        //SearchString contains the text users enter in the search text box.
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        //Requires Microsoft.Asp.NetCore.Mvc.Rendering
+        //Genres contains the list of genres. Allows the user to select from a list.
+        public SelectList Genres { get; set; }
+
+        //MovieGenre contains the specific genre the user selects.
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenere { get; set; }
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            //contains a list of movies.
+            var movies = from m in _context.Movie
+                         select m;
+
+            //completes the users search.
+            if (!string.IsNullOrEmpty(SearchString)) {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            //update the movie object from the search.
+            Movie = await movies.ToListAsync();
         }
     }
 }
